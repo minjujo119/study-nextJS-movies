6898e95797c0922eb7ee660974ebbc75f09ac35d
minjujo119 <minjujo119@gmail.com>
2025-04-10 10:56:28 -0400

    vercel 배포 준비

---
+++ components/MovieInfo.tsx
@@ -0,0 +1,23 @@
+import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movieInfo.module.css";
+
+export async function getMovie(id: string) {
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+export default async function MovieInfo({ id }: { id: string }) {
+  const movie = await getMovie(id);
+
+  return (
+    <div className={styles.container}>
+      <img className={styles.poster} src={movie.poster_path} alt="" />
+      <div>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h2 className={styles.vote}>{`⭐️ ${movie.vote_average.toFixed(1)}`}</h2>
+        <p className={styles.overview}>{movie.overview}</p>
+        <a href={movie.homepage} target={"_blank"}>Homepage 👉🏻</a>
+      </div>
+    </div>
+  )
+}
---
+++ components/MovieVideo.tsx
@@ -0,0 +1,25 @@
+import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movieVideo.module.css"
+
+async function getVideos(id: string) {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+  // throw new Error('something broke...')
+}
+
+export default async function MovieVideo({ id }: { id: string }) {
+  const videos = await getVideos(id);
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          title={video.name}
+          src={`https://youtube.com/embed/${video.key}`}
+          allowFullScreen
+        />
+      ))}
+    </div>
+  );
+}
